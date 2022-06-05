b2166633f88e488a3d43b464b119f66c4a83a570
Sr. Santos Luna <ing.santosluna@hotmail.com>
2022-06-05 17:24:40 -0400

    Actualizacion01

---
+++ appAlmacen/Comun/Genericos.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun
+{
+    public class Genericos
+    {
+        private int _Id;
+        private string _Nombre;
+        private int _IsActivo;
+
+        public int Id
+        {
+            get
+            {
+                return _Id;
+            }
+
+            set
+            {
+                _Id = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value;
+            }
+        }
+
+        public int IsActivo
+        {
+            get
+            {
+                return _IsActivo;
+            }
+
+            set
+            {
+                _IsActivo = value;
+            }
+        }
+    }
+}
---
+++ appAlmacen/Presntacion/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Presntacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtUsuario = new Guna.UI.WinForms.GunaTextBox();
+            this.txtPassword = new Guna.UI.WinForms.GunaTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.BtnAceptar = new Guna.UI.WinForms.GunaGradientButton();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.txtUsuario.BaseColor = System.Drawing.Color.White;
+            this.txtUsuario.BorderColor = System.Drawing.Color.Silver;
+            this.txtUsuario.BorderSize = 1;
+            this.txtUsuario.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsuario.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtUsuario.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtUsuario.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtUsuario.Location = new System.Drawing.Point(32, 91);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.Radius = 6;
+            this.txtUsuario.Size = new System.Drawing.Size(199, 26);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Transparent;
+            this.txtPassword.BaseColor = System.Drawing.Color.White;
+            this.txtPassword.BorderColor = System.Drawing.Color.Silver;
+            this.txtPassword.BorderSize = 1;
+            this.txtPassword.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtPassword.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtPassword.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtPassword.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtPassword.Location = new System.Drawing.Point(32, 158);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '\0';
+            this.txtPassword.Radius = 6;
+            this.txtPassword.Size = new System.Drawing.Size(199, 26);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Usuario de acceso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(29, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Usuario de acceso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Purple;
+            this.label3.Location = new System.Drawing.Point(61, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Iniciar Sesion";
+            // 
+            // BtnAceptar
+            // 
+            this.BtnAceptar.AnimationHoverSpeed = 0.07F;
+            this.BtnAceptar.AnimationSpeed = 0.03F;
+            this.BtnAceptar.BackColor = System.Drawing.Color.Transparent;
+            this.BtnAceptar.BaseColor1 = System.Drawing.Color.SlateBlue;
+            this.BtnAceptar.BaseColor2 = System.Drawing.Color.Fuchsia;
+            this.BtnAceptar.BorderColor = System.Drawing.Color.Black;
+            this.BtnAceptar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.BtnAceptar.FocusedColor = System.Drawing.Color.Empty;
+            this.BtnAceptar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.BtnAceptar.ForeColor = System.Drawing.Color.White;
+            this.BtnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("BtnAceptar.Image")));
+            this.BtnAceptar.ImageAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BtnAceptar.ImageSize = new System.Drawing.Size(20, 20);
+            this.BtnAceptar.Location = new System.Drawing.Point(51, 217);
+            this.BtnAceptar.Name = "BtnAceptar";
+            this.BtnAceptar.OnHoverBaseColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(155)))), ((int)(((byte)(145)))), ((int)(((byte)(221)))));
+            this.BtnAceptar.OnHoverBaseColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(85)))), ((int)(((byte)(255)))));
+            this.BtnAceptar.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.BtnAceptar.OnHoverForeColor = System.Drawing.Color.White;
+            this.BtnAceptar.OnHoverImage = null;
+            this.BtnAceptar.OnPressedColor = System.Drawing.Color.Black;
+            this.BtnAceptar.Radius = 12;
+            this.BtnAceptar.Size = new System.Drawing.Size(157, 39);
+            this.BtnAceptar.TabIndex = 5;
+            this.BtnAceptar.Text = "Aceptar";
+            this.BtnAceptar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(258, 319);
+            this.Controls.Add(this.BtnAceptar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaTextBox txtUsuario;
+        private Guna.UI.WinForms.GunaTextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI.WinForms.GunaGradientButton BtnAceptar;
+    }
+}
+
